2efbeb87a7193552dc999e2b91052f1da67088f8
Margaret Alade <alade.margaret@yandex.ru>
2024-02-01 06:28:31 +0300

    Initial Commit

---
+++ Task-05.03 Functors/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-05.03 Functors
+//
+//  Created by Маргарет  on 01.02.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
