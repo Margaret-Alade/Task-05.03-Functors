768e17f37cef309be96b171c5ed7180eb230b9c6
Margaret Alade <alade.margaret@yandex.ru>
2024-02-02 00:16:21 +0300

    Initial commit

--- Task-05.03 Functors/main.cpp
+++ Task-05.03 Functors/main.cpp
@@ -6,9 +6,47 @@
 //
 
 #include <iostream>
+#include<vector>
+#include<algorithm>
+
+class func_div_by_3 {
+    int sum;
+    int count;
+public:
+    func_div_by_3() : sum{0}, count{0} {};
+    
+    void operator() (int a) {
+        if (a % 3 == 0) {
+            sum = sum + a;
+            count++;
+        }
+    }
+    int get_count() {
+        return count;
+    }
+    
+    int get_sum() {
+        return sum;
+    }
+};
+
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    std::vector<int> vec {4,1, 3, 6, 25, 54};
+    
+    std::cout << "[IN]: ";
+    
+    for (int i : vec) {
+        std::cout << i << " ";
+    }
+    
+    std::cout << "\n";
+    
+    func_div_by_3 func;
+    func = std::for_each(vec.begin(), vec.end(), func_div_by_3());
+    std::cout << "[OUT]: get_sum() = " << func.get_sum() << "\n";
+    std::cout << "[OUT]: get_count() = " << func.get_count() << "\n";
+    
     return 0;
+    
 }
